3c1b86e9d01a095d59685bca4cbc96ccc6435093
gidorah <onur.accounts@gmail.com>
2017-02-16 21:27:28 +0300

    tacopie tcp socket fix applied

--- tsim_client/tsim_client.cpp
+++ tsim_client/tsim_client.cpp
@@ -4,5 +4,16 @@
 int
 main(void) {
 
+#ifdef _WIN32
+	//! Windows netword DLL init
+	WORD version = MAKEWORD(2, 2);
+	WSADATA data;
+
+	if (WSAStartup(version, &data) != 0) {
+		std::cerr << "WSAStartup() failure" << std::endl;
+		return -1;
+	}
+#endif /* _WIN32 */
+
 	Data_Controller data_controller;
 }
